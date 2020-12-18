dbb744db6dcc3f1dbbd2a443f30b55cdb2f61670
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-12-19 01:23:52 +0300

    ekleme yok

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(258, 591);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(72, 17);
-            this.linkLabel1.TabIndex = 0;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "linkLabel1";
             // 
             // Form1
             // 
@@ -50,7 +39,6 @@
             this.BackgroundImage = global::MIS.Properties.Resources.pexels_magda_ehlers_1300972;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1006, 682);
-            this.Controls.Add(this.linkLabel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(1024, 729);
             this.MinimumSize = new System.Drawing.Size(1024, 729);
@@ -59,13 +47,10 @@
             this.Text = "Giriş Yap!";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
