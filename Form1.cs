9f346ddea1ef5f6f551bd2eec31fecebe55e2707
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-12-19 22:19:01 +0300

    ilk push umuz

--- Form1.cs
+++ Form1.cs
@@ -23,17 +23,21 @@ namespace MIS
         private void Form1_Load(object sender, EventArgs e)
         {
             linkLabel1.Text = "Kaynak kodu görüntüle : GitHub" ;
+           
 
             linkLabel1.Links.Add(24, 6, "https://github.com/nskrkmz/MIS");
-            
+            linkLabel1.ForeColor = System.Drawing.Color.Aqua;
+
             //Linkleri mavi göster
-            linkLabel1.LinkColor = Color.Blue;
+            //linkLabel1.LinkColor = Color.DarkBlue;
+            linkLabel1.LinkColor = System.Drawing.ColorTranslator.FromHtml("#E48982");
             //Gezilmiş linkleri kırmızı göster
-            linkLabel1.VisitedLinkColor = Color.Red;
+            linkLabel1.VisitedLinkColor = Color.Blue;
             //Aktif linkleri kahverengi göster
             linkLabel1.ActiveLinkColor = Color.Brown;
             //Linklerin altını fare üzerine geldiğinde çiz
             linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
+            
 
         }
 
@@ -49,11 +53,12 @@ namespace MIS
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            
             //Tıklanan linki ziyaret edilmiş renkle göster
             linkLabel1.Links[linkLabel1.Links.IndexOf(e.Link)].Visited = true;
             //Linki çalıştır
             System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
-
+            
         }
     }
 }
