26e42042d2aa6727536b8c995c8d9c50ca7d5483
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-12-29 19:04:12 +0300

    Veri Tabanı bağlantısı Tanımlandı

---
+++ Entity/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MIS.Entity
+{
+    class Context:DbContext
+    {
+        public DbSet<Urun> Uruns { get; set; }
+
+    }
+}
---
+++ Entity/Urun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS.Entity
+{
+    public class Urun
+    {
+        [Key]
+        public int urunID { get; set; }//Urun tablosun key'i urunID olarak tanımlandı
+
+        public string depoTarih { get; set; }
+        public int urunAlisFiyat { get; set; }
+        public int urunGr { get; set; }
+        public string urunSTT { get; set; }
+        public int irsaliyeNo { get; set; }
+
+
+
+    }
+}
