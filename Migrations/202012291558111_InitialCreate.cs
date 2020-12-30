e079743346bf77ab7088dc8dc05ddf3ee06fb3ff
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-12-30 20:19:55 +0300

    Varlık class ları oluşturuldu/migrations eklendi

---
+++ Migrations/202012291558111_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace MIS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Uruns",
+                c => new
+                    {
+                        urunID = c.Int(nullable: false, identity: true),
+                        depoTarih = c.String(),
+                        urunAlisFiyat = c.Int(nullable: false),
+                        urunGr = c.Int(nullable: false),
+                        urunSTT = c.String(),
+                        irsaliyeNo = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.urunID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Uruns");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MIS.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MIS.Entity.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MIS.Entity.Context";
+        }
+
+        protected override void Seed(MIS.Entity.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
