b4c82f2ffe49f9f1c09e372dd6da875c12955d0c
Enes Korkmaz <aeneskrkmz@gmail.com>
2021-01-13 17:27:20 +0300

    Raporlama Pencereleri Oluşturuldu

---
+++ FormKar-Zarar(Tarih).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIS
+{
+    public partial class FormKar_Zarar_Tarih_ : Form
+    {
+        public FormKar_Zarar_Tarih_()
+        {
+            InitializeComponent();
+        }
+    }
+}
