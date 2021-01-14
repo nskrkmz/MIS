d9c2c096479ce957a346acd83dbcee0b952ea0ec
Batuhantuncdev <76177986+batuhantuncdev@users.noreply.github.com>
2021-01-14 04:43:19 +0300

    Ana Ekrana Iconlar Eklendi

--- FormAnaEkran.Designer.cs
+++ FormAnaEkran.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnaEkran));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnCikis = new System.Windows.Forms.Button();
@@ -37,12 +38,21 @@
             this.btnBorcOdeme = new System.Windows.Forms.Button();
             this.btnSatis = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList4 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList5 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList6 = new System.Windows.Forms.ImageList(this.components);
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(26)))), ((int)(((byte)(35)))));
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.btnCikis);
             this.panel1.Controls.Add(this.btnPersonelKayit);
             this.panel1.Controls.Add(this.btnRaporlama);
@@ -51,9 +61,8 @@
             this.panel1.Controls.Add(this.btnSatis);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(332, 839);
+            this.panel1.Size = new System.Drawing.Size(249, 688);
             this.panel1.TabIndex = 1;
             // 
             // btnCikis
@@ -65,10 +74,13 @@
             this.btnCikis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnCikis.ForeColor = System.Drawing.Color.White;
-            this.btnCikis.Location = new System.Drawing.Point(1, 436);
-            this.btnCikis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCikis.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCikis.ImageKey = "enter_2.png";
+            this.btnCikis.ImageList = this.ımageList1;
+            this.btnCikis.Location = new System.Drawing.Point(1, 415);
+            this.btnCikis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCikis.Name = "btnCikis";
-            this.btnCikis.Size = new System.Drawing.Size(328, 64);
+            this.btnCikis.Size = new System.Drawing.Size(246, 52);
             this.btnCikis.TabIndex = 8;
             this.btnCikis.Text = "Çıkış";
             this.btnCikis.UseVisualStyleBackColor = false;
@@ -83,10 +95,13 @@
             this.btnPersonelKayit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPersonelKayit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnPersonelKayit.ForeColor = System.Drawing.Color.White;
-            this.btnPersonelKayit.Location = new System.Drawing.Point(0, 367);
-            this.btnPersonelKayit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPersonelKayit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPersonelKayit.ImageKey = "Healthcare-Groups-icon.png";
+            this.btnPersonelKayit.ImageList = this.ımageList3;
+            this.btnPersonelKayit.Location = new System.Drawing.Point(0, 359);
+            this.btnPersonelKayit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPersonelKayit.Name = "btnPersonelKayit";
-            this.btnPersonelKayit.Size = new System.Drawing.Size(328, 64);
+            this.btnPersonelKayit.Size = new System.Drawing.Size(246, 52);
             this.btnPersonelKayit.TabIndex = 7;
             this.btnPersonelKayit.Text = "Personel Kayıt";
             this.btnPersonelKayit.UseVisualStyleBackColor = false;
@@ -101,10 +116,13 @@
             this.btnRaporlama.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRaporlama.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnRaporlama.ForeColor = System.Drawing.Color.White;
-            this.btnRaporlama.Location = new System.Drawing.Point(0, 298);
-            this.btnRaporlama.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRaporlama.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRaporlama.ImageKey = "1.png";
+            this.btnRaporlama.ImageList = this.ımageList5;
+            this.btnRaporlama.Location = new System.Drawing.Point(0, 303);
+            this.btnRaporlama.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRaporlama.Name = "btnRaporlama";
-            this.btnRaporlama.Size = new System.Drawing.Size(328, 64);
+            this.btnRaporlama.Size = new System.Drawing.Size(246, 52);
             this.btnRaporlama.TabIndex = 6;
             this.btnRaporlama.Text = "Raporlama";
             this.btnRaporlama.UseVisualStyleBackColor = false;
@@ -119,10 +137,13 @@
             this.btnStokGiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStokGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnStokGiris.ForeColor = System.Drawing.Color.White;
-            this.btnStokGiris.Location = new System.Drawing.Point(0, 229);
-            this.btnStokGiris.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStokGiris.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStokGiris.ImageKey = "2.png";
+            this.btnStokGiris.ImageList = this.ımageList6;
+            this.btnStokGiris.Location = new System.Drawing.Point(0, 247);
+            this.btnStokGiris.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStokGiris.Name = "btnStokGiris";
-            this.btnStokGiris.Size = new System.Drawing.Size(328, 64);
+            this.btnStokGiris.Size = new System.Drawing.Size(246, 52);
             this.btnStokGiris.TabIndex = 5;
             this.btnStokGiris.Text = "Stok Giriş";
             this.btnStokGiris.UseVisualStyleBackColor = false;
@@ -137,10 +158,13 @@
             this.btnBorcOdeme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBorcOdeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnBorcOdeme.ForeColor = System.Drawing.Color.White;
-            this.btnBorcOdeme.Location = new System.Drawing.Point(0, 160);
-            this.btnBorcOdeme.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBorcOdeme.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBorcOdeme.ImageKey = "Ödeme.png";
+            this.btnBorcOdeme.ImageList = this.ımageList4;
+            this.btnBorcOdeme.Location = new System.Drawing.Point(0, 191);
+            this.btnBorcOdeme.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBorcOdeme.Name = "btnBorcOdeme";
-            this.btnBorcOdeme.Size = new System.Drawing.Size(328, 64);
+            this.btnBorcOdeme.Size = new System.Drawing.Size(246, 52);
             this.btnBorcOdeme.TabIndex = 4;
             this.btnBorcOdeme.Text = "Borç Ödeme";
             this.btnBorcOdeme.UseVisualStyleBackColor = false;
@@ -155,10 +179,13 @@
             this.btnSatis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSatis.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnSatis.ForeColor = System.Drawing.Color.White;
-            this.btnSatis.Location = new System.Drawing.Point(0, 91);
-            this.btnSatis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSatis.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSatis.ImageKey = "Ecommerce-Sell-icon.png";
+            this.btnSatis.ImageList = this.ımageList2;
+            this.btnSatis.Location = new System.Drawing.Point(0, 135);
+            this.btnSatis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSatis.Name = "btnSatis";
-            this.btnSatis.Size = new System.Drawing.Size(328, 64);
+            this.btnSatis.Size = new System.Drawing.Size(246, 52);
             this.btnSatis.TabIndex = 3;
             this.btnSatis.Text = "Satış";
             this.btnSatis.UseVisualStyleBackColor = false;
@@ -166,34 +193,82 @@
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(325, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(244, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1264, 857);
+            this.panel2.Size = new System.Drawing.Size(948, 696);
             this.panel2.TabIndex = 3;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 127);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "enter_2.png");
+            // 
+            // ımageList2
+            // 
+            this.ımageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList2.ImageStream")));
+            this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList2.Images.SetKeyName(0, "Ecommerce-Sell-icon.png");
+            // 
+            // ımageList3
+            // 
+            this.ımageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList3.ImageStream")));
+            this.ımageList3.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList3.Images.SetKeyName(0, "Healthcare-Groups-icon.png");
+            // 
+            // ımageList4
+            // 
+            this.ımageList4.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList4.ImageStream")));
+            this.ımageList4.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList4.Images.SetKeyName(0, "Ödeme.png");
+            // 
+            // ımageList5
+            // 
+            this.ımageList5.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList5.ImageStream")));
+            this.ımageList5.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList5.Images.SetKeyName(0, "1.png");
+            // 
+            // ımageList6
+            // 
+            this.ımageList6.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList6.ImageStream")));
+            this.ımageList6.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList6.Images.SetKeyName(0, "2.png");
+            // 
             // FormAnaEkran
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(1587, 839);
+            this.ClientSize = new System.Drawing.Size(1192, 688);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MaximumSize = new System.Drawing.Size(1605, 886);
-            this.MinimumSize = new System.Drawing.Size(1605, 886);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(1208, 727);
+            this.MinimumSize = new System.Drawing.Size(1208, 727);
             this.Name = "FormAnaEkran";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Market Information System";
             this.Load += new System.EventHandler(this.FormAnaEkran_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -208,5 +283,12 @@
         private System.Windows.Forms.Button btnPersonelKayit;
         private System.Windows.Forms.Button btnCikis;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.ImageList ımageList2;
+        private System.Windows.Forms.ImageList ımageList3;
+        private System.Windows.Forms.ImageList ımageList4;
+        private System.Windows.Forms.ImageList ımageList5;
+        private System.Windows.Forms.ImageList ımageList6;
     }
 }
